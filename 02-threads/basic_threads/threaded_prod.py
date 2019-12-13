b987000b65c31f42d4511b2b5ff589029a386a3d
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-12-13 10:34:46 +0100

    Consumer producer program using threading

---
+++ 02-threads/basic_threads/threaded_prod.py
@@ -0,0 +1,58 @@
+import datetime
+import colorama
+import random
+import time
+import threading
+
+
+def main():
+    t0 = datetime.datetime.now()
+    print(colorama.Fore.WHITE + "App started.", flush=True)
+    data = []
+
+    threads = [
+        threading.Thread(target=generate_data, args=(20, data)),
+        threading.Thread(target=generate_data, args=(20, data)),
+        threading.Thread(target=process_data, args=(40, data)),
+    ]
+
+    print("Start threads")
+    [t.start() for t in threads]
+
+    [t.join() for t in threads]
+    print("Join threads")
+
+    dt = datetime.datetime.now() - t0
+    print(colorama.Fore.WHITE + "App exiting, total time: {:,.2f} sec.".format(dt.total_seconds()), flush=True)
+
+
+def generate_data(num: int, data: list):
+    for idx in range(1, num + 1):
+        item = idx*idx
+        data.append((item, datetime.datetime.now()))
+
+        print(colorama.Fore.YELLOW + " -- generated item {}".format(idx), flush=True)
+        time.sleep(random.random() + .5)
+
+
+def process_data(num: int, data: list):
+    processed = 0
+    while processed < num:
+        if data:
+            item = data.pop(0)
+        else:
+            time.sleep(.01)
+            continue
+
+        processed += 1
+        value = item[0]
+        t = item[1]
+        dt = datetime.datetime.now() - t
+
+        print(colorama.Fore.CYAN +
+              " +++ Processed value {} after {:,.2f} sec.".format(value, dt.total_seconds()), flush=True)
+        time.sleep(.5)
+
+
+if __name__ == '__main__':
+    main()
