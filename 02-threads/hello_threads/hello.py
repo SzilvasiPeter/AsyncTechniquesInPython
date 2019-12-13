4f4b32e74d16185ddbad71e18651f33628699aca
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-12-13 10:22:50 +0100

    Waiting more than one thread

--- 02-threads/hello_threads/hello.py
+++ 02-threads/hello_threads/hello.py
@@ -3,13 +3,19 @@ import threading
 
 
 def main():
-    t = threading.Thread(target=greeter, args=("Peter", 10), daemon=True)
-    t.start()
+    threads = [
+        threading.Thread(target=greeter, args=("Peter", 10), daemon=True),
+        threading.Thread(target=greeter, args=("Ellis", 5), daemon=True),
+        threading.Thread(target=greeter, args=("Robert", 7), daemon=True),
+        threading.Thread(target=greeter, args=("Sam", 4), daemon=True)
+    ]
+
+    [t.start() for t in threads]
 
     print("Do some other work.")
     print(2*2)
 
-    t.join()
+    [t.join(timeout=2) for t in threads]
 
     print("Done.")
 
