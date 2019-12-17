81f1304c13a0422186833e470e3039c638f241d1
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-12-17 17:53:21 +0100

    Multiprocessing returning value

--- 04-multi-processing/cpu_attempt/compute_multiprocessing.py
+++ 04-multi-processing/cpu_attempt/compute_multiprocessing.py
@@ -12,23 +12,34 @@ def main():
 
     processor_count = multiprocessing.cpu_count()
     pool = multiprocessing.Pool()
+    tasks = []
     for n in range(1, processor_count + 1):
-        pool.apply_async(do_math, (30_000_000 * (n - 1) / processor_count,
+        task = pool.apply_async(do_math, (30_000_000 * (n - 1) / processor_count,
                                    30_000_000 * n / processor_count))
+
+        tasks.append(task)
 
     pool.close()
     pool.join()
+    print("Our result: ")
+    for t in tasks:
+        print(t.get())
 
     dt = datetime.datetime.now() - t0
     print("Done in {:,.2f} sec.".format(dt.total_seconds()))
 
 
-def do_math(start=0, num=10):
+def do_math(start=0, num=10) -> int:
     pos = start
     k_sq = 1000 * 1000
+    sum = 0
     while pos < num:
         pos += 1
-        dist = math.sqrt((pos - k_sq) * (pos - k_sq))
+        value = math.sqrt((pos - k_sq) * (pos - k_sq))
+        sum += value
+
+    avarage = sum / num
+    return int(avarage)
 
 
 if __name__ == '__main__':
